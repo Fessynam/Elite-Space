d60063148a8743021c8cce89f12ff74d08e5d055
LordKali11 <barkiasshapaka@gmail.com>
2024-07-20 15:33:40 +0200

    Update image_constant.dart

--- smart_health_home/lib/core/utils/image_constant.dart
+++ smart_health_home/lib/core/utils/image_constant.dart
@@ -1,1 +1,10 @@
 // TODO Implement this library.
+
+class ImageConstant{
+  static String imagePath = 'assets/images/';
+  static String imgCardMeal = '$imagePath/img_card_meal.svg';
+  static String imgFloatingIcon = '$imagePath/img_floating_icon.svg';
+  static String imgFloatingIconBlack900 = '$imagePath/img_floating_icon_black_900.svg';
+  static String imgPolygon1 = '$imagePath/img_polygon_1.svg';
+  static String imgNotFound = 'assest/images/image_not_found.png';
+}
